d0ff6d2932eee5f8189e9785c512c36bf15f1bb4
Aeo Zatoichi Bax <109118450+im-aeo@users.noreply.github.com>
2025-07-23 21:19:14 -0400

    Update Hit.go

--- Hit.go
+++ Hit.go
@@ -5,7 +5,7 @@ import(
 )
 // Hit represents information about a ray's intersection with a surface.
 type Hit struct {
-    Point    Vector // The point of intersection in 3D space
+    Points    Vector // The point of intersection in 3D space
     Normal   Vector // The normal vector at the intersection point
     //Material *Material // Material properties at the intersection point (e.g., color, reflectivity)
     // You can add more fields as needed, such as texture coordinates, distance, etc.

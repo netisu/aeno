5b011925d5d5cb159ff60d61088fe6cce377f2fb
Aeo Zatoichi Bax <109118450+im-aeo@users.noreply.github.com>
2025-08-13 22:55:07 -0400

    Update SolidShader.go

--- SolidShader.go
+++ SolidShader.go
@@ -1,1 +1,22 @@
+package aeno
 
+import "github.com/go-gl/mathgl/mgl64"
+
+type SolidColorShader struct {
+	Matrix Matrix // View-Projection matrix
+	Color  Color
+}
+
+func (s *SolidColorShader) GetMatrix() Matrix {
+	return s.Matrix
+}
+
+func (s *SolidColorShader) Vertex(v Vertex, modelMatrix mgl64.Mat4) Vertex {
+	mvp := s.Matrix.Mul(modelMatrix)
+	v.Output = mvp.MulPositionW(v.Position)
+	return v
+}
+
+func (s *SolidColorShader) Fragment(v Vertex, fromObject *Object) Color {
+	return s.Color
+}
